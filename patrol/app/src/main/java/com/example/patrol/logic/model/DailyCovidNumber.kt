6fa2f2252b04793122f81e98dbdc4b70a2f2401f
marcosu0412 <57587293+marcosu0412@users.noreply.github.com>
2024-04-17 23:08:12 -0700

    Add viewModel for history data

---
+++ patrol/app/src/main/java/com/example/patrol/logic/model/DailyCovidNumber.kt
@@ -0,0 +1,6 @@
+package com.example.patrol.logic.model
+
+data class DailyCovidNumber (
+    var date: String,
+    var number: Float
+)
--- patrol/app/src/main/java/com/example/patrol/view/charts/BarChartActivity.kt
+++ patrol/app/src/main/java/com/example/patrol/view/charts/BarChartActivity.kt
@@ -2,6 +2,8 @@ package com.example.patrol.view.charts
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.patrol.R
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.XAxis
@@ -13,43 +15,39 @@ import com.github.mikephil.charting.formatter.ValueFormatter
 import java.util.*
 
 class BarChartActivity : AppCompatActivity() {
+    private lateinit var viewModel: BarChartViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bar_chart)
         val barChart = findViewById<BarChart>(R.id.idBarChart)
 
-        val entries = ArrayList<BarEntry>()
-        entries.add(BarEntry(0f, 20f))
-        entries.add(BarEntry(1f, 30f))
-        entries.add(BarEntry(2f, 40f))
-        entries.add(BarEntry(3f, 50f))
-        entries.add(BarEntry(4f, 60f))
+        viewModel = ViewModelProvider(this).get(BarChartViewModel::class.java)
 
-        val barDataSet = BarDataSet(entries, "Bar Data Set")
-        barDataSet.color = Color.BLUE
+        viewModel.barEntries.observe(this) { entries ->
+            val dataSet = BarDataSet(entries, "Bar Data Set")
+            val data = BarData(dataSet)
+            barChart.data = data
+            barChart.data.setValueTextSize(18f)
 
-        val barData = BarData(barDataSet)
-        barChart.data = barData
+            val xAxis = barChart.xAxis
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            xAxis.setDrawGridLines(false)
+            xAxis.textSize = 18f
 
-        barChart.data.setValueTextSize(18f)
 
-        val xAxis = barChart.xAxis
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.setDrawGridLines(false)
-        xAxis.textSize = 20f
+            barChart.setFitBars(true)
+            barChart.animateY(1500)
 
-        val labels = ArrayList<String>()
-        labels.add("01-01")
-        labels.add("01-02")
-        labels.add("01-03")
-        labels.add("01-04")
-        labels.add("01-05")
+            barChart.invalidate()
+        }
 
-        xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        viewModel.xAxisLabels.observe(this) { labels ->
+            val xAxis = barChart.xAxis
+            xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        }
 
-        barChart.setFitBars(true)
-        barChart.animateY(1500)
-        barChart.invalidate()
+        viewModel.generateBarEntries()
     }
+
 
 }
