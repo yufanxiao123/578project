9025b5e2264b9bd80ec194d25d50303906822ced
JasonGuojz <jzguovulcan@gmail.com>
2024-04-20 23:45:12 -0700

    prediction

--- patrol/app/src/main/java/com/example/patrol/logic/Repository.kt
+++ patrol/app/src/main/java/com/example/patrol/logic/Repository.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.liveData
 import com.example.patrol.logic.model.News
 import com.example.patrol.logic.network.PatrolNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
 object Repository {
     fun getNews(query : String?) = fire(Dispatchers.IO) {
@@ -15,6 +18,17 @@ object Repository {
     fun getCrowd(lat: String, lon: String) = fire(Dispatchers.IO) {
         val multiCrowds = PatrolNetwork.getCrowd(lat,lon)
         Result.success(multiCrowds)
+    }
+
+    fun getPrediction(lat: Double, lon: Double) = fire(Dispatchers.IO) {
+        coroutineScope {
+            val deferredPrediction = async {
+                PatrolNetwork.getPrediction(lat, lon)
+            }
+            val multiPredictions = deferredPrediction.await()
+            Result.success(multiPredictions)
+        }
+
     }
 
     private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
---
+++ patrol/app/src/main/java/com/example/patrol/logic/model/Predictions.kt
@@ -0,0 +1,6 @@
+package com.example.patrol.logic.model
+
+data class Predictions(
+    val time: String = "Sample Time",
+    val crowdness: Float = 0.0f,
+)
