0ce84d69d24f79a131abb6358a1c83723d100eda
marcosu0412 <57587293+marcosu0412@users.noreply.github.com>
2024-04-24 19:37:49 -0700

    Set up the interface with backend, modified barCharActivity

--- patrol/app/src/main/java/com/example/patrol/view/charts/BarChartActivity.kt
+++ patrol/app/src/main/java/com/example/patrol/view/charts/BarChartActivity.kt
@@ -2,10 +2,12 @@ package com.example.patrol.view.charts
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
 import com.example.patrol.R
+import com.example.patrol.logic.Repository
+import com.example.patrol.logic.model.DailyCovidNumber
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.BarLineChartBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
@@ -14,39 +16,68 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
 import java.util.*
 
+
 class BarChartActivity : AppCompatActivity() {
-    private lateinit var viewModel: BarChartViewModel
+    private lateinit var barChart: BarChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bar_chart)
-        val barChart = findViewById<BarChart>(R.id.idBarChart)
+        barChart = findViewById<BarChart>(R.id.idBarChart)
+        observeDataChanges()
+    }
 
-        viewModel = ViewModelProvider(this).get(BarChartViewModel::class.java)
+    private fun observeDataChanges() {
+        Repository.getHistroyData().observe(this){ data ->
+            data.getOrNull().let {
+                if (it != null) {
+                    displayBarChart(it)
+                }
+            }
+        }
+    }
 
-        viewModel.barEntries.observe(this) { entries ->
-            val dataSet = BarDataSet(entries, "Bar Data Set")
-            val data = BarData(dataSet)
-            barChart.data = data
-            barChart.data.setValueTextSize(18f)
-
-            val xAxis = barChart.xAxis
-            xAxis.position = XAxis.XAxisPosition.BOTTOM
-            xAxis.setDrawGridLines(false)
-            xAxis.textSize = 18f
-
-
-            barChart.setFitBars(true)
-            barChart.animateY(1500)
-
-            barChart.invalidate()
+    private fun displayBarChart(dailyCovidNumbers: List<DailyCovidNumber?>) {
+        val barEntries = mutableListOf<BarEntry>()
+        val dates = mutableListOf<String>()
+        dailyCovidNumbers.forEachIndexed { index, dailyCovidNumber ->
+            if (dailyCovidNumber != null) {
+                barEntries.add(BarEntry(index.toFloat(), dailyCovidNumber.number))
+            }
+            if (dailyCovidNumber != null) {
+                dates.add(dailyCovidNumber.date)
+            }
         }
 
-        viewModel.xAxisLabels.observe(this) { labels ->
-            val xAxis = barChart.xAxis
-            xAxis.valueFormatter = IndexAxisValueFormatter(labels)
-        }
+        val barDataSet = BarDataSet(barEntries, "Data")
+        barDataSet.color = Color.BLUE
+        barDataSet.valueTextSize = 20f // Set text size for data values
+        val barData = BarData(barDataSet)
 
-        viewModel.generateBarEntries()
+
+        // Customize x-axis labels
+        val xAxis: XAxis = barChart.getXAxis()
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.setDrawGridLines(false)
+        xAxis.setGranularity(1f) // only intervals of 1 day
+        xAxis.textSize = 18f
+        xAxis.setLabelCount(5)
+        xAxis.valueFormatter = IndexAxisValueFormatter(dates)
+
+        // Add animation
+        barChart.animateY(1500, Easing.EaseInOutQuart)
+        barChart.setTouchEnabled(true)
+        barChart.setDragEnabled(true)
+        barChart.setScaleEnabled(true)
+
+        barChart.data = barData
+        barChart.invalidate()
+    }
+
+
+    class DayAxisValueFormatter(private val chart: BarLineChartBase<*>) : ValueFormatter() {
+        override fun getFormattedValue(value: Float): String {
+            return "MM-DD"
+        }
     }
 
 
